a0ea7a35637c6bbc5738ebba067c7492101214e0
Syoshi7 <155185064+Syoshi7@users.noreply.github.com>
2025-01-23 03:39:53 +0400

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+public class MyString
+{
+    private string _text;
+
+    public string Text
+    {
+        get => _text;
+        set
+        {
+            if (value == null)
+            {
+                throw new ArgumentException("Без пустых строк");
+            }
+            else
+            {
+                _text = value;
+            }
+        }
+    }
+    public MyString()
+    {
+        Text = "Default";
+    }
+
+    public MyString(string text)
+    {
+        if (text == null)
+        {
+            throw new ArgumentException("Без пустых строк");
+        }
+        Text = text;
+    }
+
+    public MyString(MyString other)
+    {
+        if (other.Text == null)
+            throw new ArgumentException("Без пустых строк");
+        Text = other.Text;
+    }
+
+
+    public string GetFirstAndLast()
+    {
+        if (string.IsNullOrEmpty(Text))
+            return "Строка пуста";
+
+        return Text.Length == 1 ? Text + Text : $"{Text[0]}{Text[^1]}";
+    }
+
+
+    public override string ToString()
+    {
+        return $"Text: {Text}";
+    }
+}
+
+
+public class ExtendedString : MyString
+{
+    private int _length;
+
+    public int Lenght
+    {
+        get => _length;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Длина строки не может быть отрицательной");
+            _length = value;
+        }
+    }
+
+    public ExtendedString() : base(" ")
+    {
+        Lenght = 1;
+    }
+
+    public ExtendedString(string text, int length) 
+        : base(text)
+    {
+        Lenght = length;
+    }
+
+
+    public ExtendedString(ExtendedString other) : base(other)
+    {
+        Lenght = other.Lenght;
+    }
+
+
+    public int CountVowels()
+    {
+        int count = 0;
+        string vowels = "AEIOUaeiouАЕЁИОУЫЭЮЯаеёиоуыэюя";
+
+        foreach (char c in Text)
+        {
+            if (vowels.Contains(c))
+                count++;
+        }
+
+        return count;
+    }
+
+  
+    public string ReverseText()
+    {
+        char[] charArray = Text.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+
+    public override string ToString()
+    {
+        return $"Text: {Text}, Length: {Lenght}";
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Lab2
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+
+                //Тесты конструкторов
+                MyString string1 = new MyString();
+                string1.Text = "Строка1";
+
+                MyString string2 = new MyString("Строка2");
+                MyString string3 = new MyString(string2);
+
+                //Тесты методов String
+                Console.WriteLine("\nВывод всех имеющихся строк: ");
+                Console.WriteLine($"MyString1: {string1}");
+                Console.WriteLine($"MyString2: {string2}");
+                Console.WriteLine($"MyString3: {string3}");
+                Console.WriteLine("\n\nВывод первого и последнего символа каждой строки: ");
+                Console.WriteLine($"{string1.GetFirstAndLast()}, {string2.GetFirstAndLast()}, {string3.GetFirstAndLast()}");
+
+                //тесты дочернего класса ExtendedString
+                ExtendedString EString1 = new ExtendedString();
+                EString1.Text = "РасширеннаяСтрока1";
+                EString1.Lenght = 18;
+
+                ExtendedString EString2 = new ExtendedString("РасширеннаяСтрока2", 18);
+                Console.WriteLine("\nВывод всех имеющихся расширенных строк: ");
+                Console.WriteLine($"EString1: {EString1}");
+                Console.WriteLine($"EString2: {EString2}");
+
+                Console.WriteLine($"Гласные в Estring1: {EString1.CountVowels()}");
+                Console.WriteLine($"Гласные в Estring2: {EString2.CountVowels()}");
+
+                Console.WriteLine($"Текст наоборот в Estring1: {EString1.ReverseText()}");
+                Console.WriteLine($"Текст наоборот в Estring2: {EString2.ReverseText()}");
+
+                Console.WriteLine($"Первый и последний символ в EString 1: {EString1.GetFirstAndLast()}");
+                Console.WriteLine($"Первый и последний символ в EString 2: {EString2.GetFirstAndLast()}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Получено и обработано исключение неправильного аргумента: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Получено и обработано исключение: {ex.Message}");
+            }
+        }
+    }
+}
